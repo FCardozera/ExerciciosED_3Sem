052898195f74194b44daeb8d8637692508ed77cb
FCardozera <felipebcardoso0711@outlook.com>
2023-04-09 17:42:44 -0300

    Solução exercício 2

--- src/main/java/exercicios/marco1/atvd02/Main.java
+++ src/main/java/exercicios/marco1/atvd02/Main.java
@@ -1,5 +1,15 @@
 package exercicios.marco1.atvd02;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+        VetorAlunos alunos = new VetorAlunos(4);
+        Aluno aluno1 = new Aluno("João", 20, 10);
+        Aluno aluno2 = new Aluno("Robson", 22, 8);
+
+        System.out.println(alunos.adiciona(aluno1)); 
+        System.out.println(alunos.adiciona(aluno2)); 
+        System.out.println(alunos.contem(aluno2));
+        System.out.println("Tamanho: " + alunos.getTamanhoAtual());
+    }
 }
--- src/main/java/exercicios/marco1/atvd02/VetorAlunos.java
+++ src/main/java/exercicios/marco1/atvd02/VetorAlunos.java
@@ -1,13 +1,41 @@
 package exercicios.marco1.atvd02;
 
 public class VetorAlunos {
-    Aluno[] alunos;
+    private Aluno[] alunos;
+    private int tamanhoVetor = 0;
+    private int tamanhoAtual = 0;
 
     public VetorAlunos(int tamanhoVetor) {
         alunos = new Aluno[tamanhoVetor];
+        this.tamanhoVetor = tamanhoVetor;
     }
 
-    
+    public boolean contem(Aluno aluno) {
+        for (int i = 0; i < alunos.length; i++) {
+            if (alunos[i].equals(aluno)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean adiciona(Aluno aluno) {
+        if (tamanhoAtual < tamanhoVetor) {
+            this.alunos[this.tamanhoAtual] = aluno;
+            tamanhoAtual++;
+            return true;
+        }
+        return false;
+        
+    }
+
+    public int getTamanhoAtual() {
+        return tamanhoAtual;
+    }
+
+    public int getTamanhoVetor() {
+        return tamanhoVetor;
+    }
 
 
 }
