b66a00ebe994d79a1a8a69d3d34687d8f3d2a9a8
FCardozera <felipebcardoso0711@outlook.com>
2023-05-23 20:32:49 -0300

    Questão 03 - P1

---
+++ src/main/java/exercicios/marco1/exercicios_p1/questao03/FiboMain.java
@@ -0,0 +1,21 @@
+package exercicios.marco1.exercicios_p1.questao03;
+
+public class FiboMain {
+    public static void main(String[] args) {
+        System.out.println(fiboN(0));
+        System.out.println(fiboN(1));
+        System.out.println(fiboN(2));
+        System.out.println(fiboN(3));
+        System.out.println(fiboN(4));
+    }
+
+    public static int fiboN(int posicao) {
+        if (posicao == 0) {
+            return 0;
+        } else if (posicao == 1) {
+            return 1;
+        } else {
+            return (fiboN(posicao - 1) + fiboN(posicao - 2));
+        }
+    }
+}
